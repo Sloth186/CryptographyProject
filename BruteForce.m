62531cd18e130a465cd89a7b0c94cd852399794e
Sloth186 <rothpm24@gmail.com>
2025-04-11 12:44:54 -0500

    Update import link

--- BruteForce.m
+++ BruteForce.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
-Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/VigenereCipherAlgorithmShen.m"]
+Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/VigenereCipherShen.m"]
 
 
 cipherText = VigenereCipher["OLE THE LION", "UM"];
