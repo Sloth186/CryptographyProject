2435e28f76b3900b1a5bf00263d18422b67cd155
Sloth186 <rothpm24@gmail.com>
2025-04-11 12:42:31 -0500

    Minor edits

---
+++ BruteForce.m
@@ -0,0 +1,6 @@
+(* ::Package:: *)
+
+Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/VigenereCipherAlgorithmShen.m"]
+
+
+cipherText = VigenereCipher["OLE THE LION", "UM"];
