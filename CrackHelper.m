e6d54358abbc688835fa7c6d6f999f35d334c803
Sloth186 <rothpm24@gmail.com>
2025-04-16 12:07:27 -0500

    Modified -grams frequencies to be in proportions

--- CrackHelper.m
+++ CrackHelper.m
@@ -23,10 +23,10 @@ numWords
 numChars
 
 
-(* Counts the number of times each character appears across all text and sorts by said characters *)
-monoFreqAll = KeySort[KeyMap[List, Counts[Characters[StringJoin[text]]]]]
+(* Counts the number of times each character appears across all text and sorts by said characters and divides by the total number of characters *)
+monoFreqAll = KeySort[KeyMap[List, Map[#/numChars&, Counts[Characters[StringJoin[text]]]]]];
 (* Obtains frequencies only for the standard 26 alphabetic letters *)
-monoFreqAlpha = KeySelect[monoFreqAll, !MemberQ[LetterNumber[#], 0]&]
+monoFreqAlpha = KeySelect[monoFreqAll, !MemberQ[LetterNumber[#], 0]&];
 
 
 (* Bar chart to visualize monogram frequencies *)
@@ -40,8 +40,8 @@ BarChart[monoFreqAlpha, ChartLabels->Keys[monoFreqAlpha]]
 bigrams = Flatten[Subsequences[#, {2}]& /@ Select[Characters[text], Length[#]>1&], 1]
 
 
-(* Count the frequency of each digram *)
-biFreqAll = KeySort[Counts[bigrams]];
+(* Count the frequency of each digram, divided by the total number of bigrams *)
+biFreqAll = KeySort[Map[#/Length[bigrams]&, Counts[bigrams]]];
 (* Keep only digrams containing strictly alphabetical characters *)
 biFreqAlpha = KeySelect[biFreqAll, !MemberQ[LetterNumber[#], 0]&];
 
@@ -51,45 +51,41 @@ top25biFreqAlpha = Sort[biFreqAlpha, Greater][[1;;25]]
 BarChart[top25biFreqAlpha, ChartLabels->StringJoin/@Keys[top25biFreqAlpha]]
 
 
-Clear[bigrams]
-
-
+(* Convert each word in the text to a list of characters *)
+(* Select only the lists that have strictly more than 2 elements - in other words, drop 1-letter and 2-letter words *)
+(* Obtain all trigrams for each word *)
+(* The above produces a list of lists, so flatten at the first level to make counting easier *)
 trigrams = Flatten[Subsequences[#, {3}]& /@ Select[Characters[text], Length[#]>2&], 1]
 
 
-triFreqAll = KeySort[Counts[trigrams]];
+(* Count the frequency of each trigram, divided by the total number of trigrams *)
+triFreqAll = KeySort[Map[#/Length[trigrams]&, Counts[trigrams]]];
+(* Keep only trigrams containing strictly alphabetical characters *)
 triFreqAlpha = KeySelect[triFreqAll, !MemberQ[LetterNumber[#], 0]&];
 
 
-Sort[triFreqAlpha, Greater]
-
-
-Clear[trigrams]
-
-
+(* Convert each word in the text to a list of characters *)
+(* Select only the lists that have strictly more than 3 elements - in other words, drop 1-letter, 2-letter, and 3-letter words *)
+(* Obtain all tetragrams for each word *)
+(* The above produces a list of lists, so flatten at the first level to make counting easier *)
 tetragrams = Flatten[Subsequences[#, {4}]& /@ Select[Characters[text], Length[#]>3&], 1]
 
 
-tetraFreqAll = KeySort[Counts[tetragrams]];
+(* Count the frequency of each tetragram, divided by the total number of tetragrams *)
+tetraFreqAll = KeySort[Map[#/Length[tetragrams]&, Counts[tetragrams]]];
+(* Keep only tetragrams containing strictly alphabetical characters *)
 tetraFreqAlpha = KeySelect[tetraFreqAll, !MemberQ[LetterNumber[#], 0]&];
 
 
-Sort[tetraFreqAlpha, Greater]
-
-
+(* Keep the 25 most frequent tetragrams and graph on a bar chart *)
 top25tetra = Take[Sort[tetraFreqAlpha, Greater], 25]
 BarChart[top25tetra, ChartLabels -> Placed[StringJoin /@ Keys[top25tetra], Below, Rotate[#, 60 Degree]&]]
 
 
-tetraFreq = tetraFreqAlpha;
-Do[tetraFreq[[key]] /= (numChars - 3)
-, {key, Keys[tetraFreq]}]
-
-
-(*Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\monoFreqAlpha.txt", Compress[monoFreqAlpha]]*)
-(*Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\biFreqAlpha.txt", Compress[biFreqAlpha]]*)
-(*Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\triFreqAlpha.txt", Compress[triFreqAlpha]]*)
-(*Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\tetraFreqAlpha.txt", Compress[tetraFreqAlpha]]*)
+Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\monoFreqAlpha.txt", Compress[monoFreqAlpha]]
+Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\biFreqAlpha.txt", Compress[biFreqAlpha]]
+Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\triFreqAlpha.txt", Compress[triFreqAlpha]]
+Export["C:\\Users\\scrot\\Documents\\St. Olaf - Junior\\Cryptography\\FinalProject\\tetraFreqAlpha.txt", Compress[tetraFreqAlpha]]
 
 
 Fitness[text_] := Module[{},
@@ -97,13 +93,10 @@ Fitness[text_] := Module[{},
 ]
 
 
-mono = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/monoFreqAlpha.txt"]]
-bi = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/biFreqAlpha.txt"]]
-tri = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/triFreqAlpha.txt"]]
-tetra = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/tetraFreqAlpha.txt"]]
+mono = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/monoFreqAlpha.txt"]];
+bi = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/biFreqAlpha.txt"]];
+tri = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/triFreqAlpha.txt"]];
+tetra = Uncompress[Import["https://raw.githubusercontent.com/Sloth186/CryptographyProject/refs/heads/main/tetraFreqAlpha.txt"]];
 
 
-fourgrams = AssociateTo[tetra, AssociateTo[tri, AssociateTo[bi, mono]]]
-
-
-fourgrams["a"]
+fourgrams = Sort[AssociateTo[tetra, AssociateTo[tri, AssociateTo[bi, mono]]], Greater]
